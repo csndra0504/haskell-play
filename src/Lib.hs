49c766503c04645d18bde911dd83475a47e758ec
Cassandra Wilcox <csndra0504@gmail.com>
2017-01-29 15:38:52 -0500

    refactor: all functions combined in Lab.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
     ( someFunc
+    , hello
+    , addTwo
     ) where
+
+import System.Environment
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+hello :: IO ()
+hello = putStrLn "Hello"
+--hello = do
+--    args <- getArgs
+--    putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
+
+addTwo :: IO ()
+addTwo = putStrLn "1 + 2 = 3"
+--addTwo = do
+--    arg <- getArgs
+--    let arg1 = arg !! 0
+--    let arg2 = arg !! 1
+--    let sum = (read arg1) + (read arg2)
+--    putStrLn (arg1 ++ " + " ++ arg2 ++ " = " ++ show sum)
