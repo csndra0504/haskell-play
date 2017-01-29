c1f3de7bbd65a625d5c3172f358031e3bda31ab1
Cassandra Wilcox <csndra0504@gmail.com>
2017-01-28 19:53:02 -0500

    feat(hello_you): print sum of two arguments

--- hello_you/hello_you.hs
+++ hello_you/hello_you.hs
@@ -3,5 +3,8 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
+    arg <- getArgs
+    let arg1 = arg !! 0
+    let arg2 = arg !! 1
+    let sum = (read arg1) + (read arg2)
+    putStrLn (arg1 ++ " + " ++ arg2 ++ " = " ++ show sum)
